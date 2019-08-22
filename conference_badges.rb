17b18bbf5dbbeceb81a712065c2d399d47800af3
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-22 14:25:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+require "pry"
+
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map {|name| badge_maker(name)}
+end
+
+def assign_rooms (names)
+  assigned = []
+  names.each_with_index do |name, index|
+  assigned << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+  return assigned
+end
+def printer
+
+end
+printer(names)
+  
